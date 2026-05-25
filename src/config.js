49c57aad7b583c84f15c5b0dccebe007db163f84
flonad <florian.nadaud@serli.com>
2026-05-25 16:32:20 +0200

    update search params

--- src/config.js
+++ src/config.js
@@ -7,22 +7,13 @@ module.exports = {
         "catalog[]": "2319",
         "price_to": "30",
         "currency": "EUR",
-        "order": "newest_first",
+        "order": "relevance",
         "language_book_ids[]": "6436"
     },
     SEARCHES: [
         { search_text: "La guerre du pavot" },
         { search_text: "La cité de jade" },
-        {
-            search_text: "The everlasting",
-            "price_to": "15",
-            "language_book_ids[]": "6435"
-        },
-        {
-            search_text: "The poet empress",
-            "price_to": "15",
-            "language_book_ids[]": "6435"
-        }
+        { search_text: "Le livre des terres bannies" },
     ],
     DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL,
     CRON_INTERVAL: process.env.CRON_INTERVAL || "*/30 * * * *",
